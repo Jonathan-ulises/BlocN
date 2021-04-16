752fc7ab2dd394b9d1d197757e8f69c9e1947093
Jonathan Sanchez <jonulisanpar@gmail.com>
2021-04-16 00:03:54 -0500

    Commit Inicial

---
+++ app/src/main/java/com/macrobios/blocn/MainActivity.java
@@ -0,0 +1,34 @@
+package com.macrobios.blocn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent toChange = new Intent(MainActivity.this, Principal.class);
+                startActivity(toChange);
+                finish();
+            }
+        }, 4000);
+
+
+    }
+}
