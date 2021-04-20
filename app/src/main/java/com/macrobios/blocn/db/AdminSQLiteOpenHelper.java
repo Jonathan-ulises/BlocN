bee0b976621e5dcb7780e574026abbdaed333da0
Jonathan Sanchez <jonulisanpar@gmail.com>
2021-04-20 13:38:06 -0500

    SQLite Implementation

---
+++ app/src/main/java/com/macrobios/blocn/db/AdminSQLiteOpenHelper.java
@@ -0,0 +1,28 @@
+package com.macrobios.blocn.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class AdminSQLiteOpenHelper extends SQLiteOpenHelper{
+
+    public AdminSQLiteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    //db = Nombre de la base da datos
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE notes (" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "title TEXT, " +
+                "note TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
