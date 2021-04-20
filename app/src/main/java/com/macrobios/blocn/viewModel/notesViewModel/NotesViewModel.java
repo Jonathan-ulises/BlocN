b2f79ac76c05950b35215910830635bdeaa9e755
Jonathan Sanchez <jonulisanpar@gmail.com>
2021-04-20 16:52:17 -0500

    notes query was implemented

---
+++ app/src/main/java/com/macrobios/blocn/viewModel/notesViewModel/NotesViewModel.java
@@ -0,0 +1,44 @@
+package com.macrobios.blocn.viewModel.notesViewModel;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.widget.Toast;
+
+import androidx.lifecycle.ViewModel;
+
+import com.macrobios.blocn.db.AdminSQLiteOpenHelper;
+import com.macrobios.blocn.model.Note;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NotesViewModel extends ViewModel {
+
+    private List<Note> listNotes;
+
+    public List<Note> getListNotes(Context ctx){
+        List<Note> mNotesList = new ArrayList<>();
+
+        AdminSQLiteOpenHelper admin =
+                new AdminSQLiteOpenHelper(ctx, "BlocNDB", null, 1);
+
+        SQLiteDatabase baseDatos = admin.getReadableDatabase();
+        Cursor fila = baseDatos.rawQuery("SELECT * FROM notes", null);
+
+        if(fila.moveToFirst()){
+            while(fila.moveToNext()){
+                Note objN = new Note();
+                objN.setTitle(fila.getString(fila.getColumnIndex("title")));
+                objN.setNoteBody(fila.getString(fila.getColumnIndex("note")));
+                mNotesList.add(objN);
+            }
+            baseDatos.close();
+        }else{
+            Toast.makeText(ctx, "Notes Empty", Toast.LENGTH_SHORT).show();
+            baseDatos.close();
+        }
+
+        return mNotesList;
+    }
+}
