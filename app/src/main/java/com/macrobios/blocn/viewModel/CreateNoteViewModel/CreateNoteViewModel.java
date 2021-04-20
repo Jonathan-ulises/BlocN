fdd03665ef3f7bc97c069a0df7ce199973a11eed
Jonathan Sanchez <jonulisanpar@gmail.com>
2021-04-19 23:49:21 -0500

    ViewModel Create

---
+++ app/src/main/java/com/macrobios/blocn/viewModel/CreateNoteViewModel/CreateNoteViewModel.java
@@ -0,0 +1,14 @@
+package com.macrobios.blocn.viewModel.CreateNoteViewModel;
+
+import androidx.lifecycle.ViewModel;
+
+public class CreateNoteViewModel extends ViewModel {
+
+    private int numCharacter;
+
+    public int getNumCharacter(){return numCharacter;}
+
+    public void addCharacter(){
+        numCharacter++;
+    }
+}
