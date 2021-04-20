15637a479194e88077af584454628fbf57f035a8
Jonathan Sanchez <jonulisanpar@gmail.com>
2021-04-20 17:59:36 -0500

    OnLongClickListener Implemented

--- app/src/main/java/com/macrobios/blocn/rcvAdapter/MyNoteAdapter.java
+++ app/src/main/java/com/macrobios/blocn/rcvAdapter/MyNoteAdapter.java
@@ -1,10 +1,16 @@
 package com.macrobios.blocn.rcvAdapter;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.macrobios.blocn.R;
@@ -33,6 +39,16 @@ public class MyNoteAdapter extends RecyclerView.Adapter<MyNoteAdapter.ViewHolder
         holder.mItem = mNoteList.get(position);
         holder.tvTitle.setText(holder.mItem.getTitle());
         holder.tvNote.setText(holder.mItem.getNoteBody());
+        /*
+        holder.imgbtnEdit.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(holder.mView.getContext(), "Click UwU", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+         */
     }
 
     @Override
@@ -44,6 +60,8 @@ public class MyNoteAdapter extends RecyclerView.Adapter<MyNoteAdapter.ViewHolder
         public final View mView;
         public final TextView tvTitle;
         public final TextView tvNote;
+        public final ImageButton imgbtnEdit;
+        public final ImageButton imgbtnDelete;
         public Note mItem;
 
         public ViewHolder(View view) {
@@ -52,6 +70,46 @@ public class MyNoteAdapter extends RecyclerView.Adapter<MyNoteAdapter.ViewHolder
             mView = view;
             tvTitle = view.findViewById(R.id.tvTitle);
             tvNote = view.findViewById(R.id.tvNote);
+            imgbtnEdit = view.findViewById(R.id.imgbtnEdit);
+            imgbtnDelete = view.findViewById(R.id.imgbtnDelete);
+
+            imgbtnEdit.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    NavController navController = Navigation.findNavController(mView);
+                    navController.navigate(R.id.action_FirstFragment_to_SecondFragment);
+                    return false;
+                }
+            });
+
+            imgbtnDelete.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    AlertDialog dialog = createDialog(v).create();
+                    dialog.show();
+                    return false;
+                }
+            });
+        }
+
+        public AlertDialog.Builder createDialog(View v){
+            AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+            builder.setTitle("Delete");
+            builder.setMessage("Are you sure?");
+            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(v.getContext(), "U_U", Toast.LENGTH_SHORT).show();
+                }
+            });
+            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(v.getContext(), "Nice :V", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            return builder;
         }
     }
 }
