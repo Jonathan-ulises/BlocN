c6a4e6f383919f1e90d865c3e65f575941eea8a9
Jonathan Sanchez <jonulisanpar@gmail.com>
2021-04-20 16:46:27 -0500

    RecycleView Implementation

--- app/src/main/java/com/macrobios/blocn/Notes.java
+++ app/src/main/java/com/macrobios/blocn/Notes.java
@@ -7,19 +7,29 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.macrobios.blocn.rcvAdapter.MyNoteAdapter;
+import com.macrobios.blocn.viewModel.notesViewModel.NotesViewModel;
 
 public class Notes extends Fragment implements View.OnClickListener{
 
     NavController navController;
+    RecyclerView rcvNotes;
+    MyNoteAdapter adapterNotes;
+    NotesViewModel notesViewModel;
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-
+        notesViewModel = new ViewModelProvider(this).get(NotesViewModel.class);
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_note, container, false);
     }
@@ -31,12 +41,10 @@ public class Notes extends Fragment implements View.OnClickListener{
 
         Principal.fab.setOnClickListener(this::onClick);
 
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                navController.navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-        });
+        rcvNotes = view.findViewById(R.id.rcvNotes);
+        rcvNotes.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+        adapterNotes = new MyNoteAdapter(notesViewModel.getListNotes(getContext()));
+        rcvNotes.setAdapter(adapterNotes);
     }
 
     @Override
